f509b7de34139c462893e9d9045814bd8a155926
mrFlick72 <valerio.vaudi@gmail.com>
2019-09-21 18:31:43 +0200

    load properties file as map from AWS S3

--- src/main/kotlin/it/valeriovaudi/i18nmessage/I18nMessageApplication.kt
+++ src/main/kotlin/it/valeriovaudi/i18nmessage/I18nMessageApplication.kt
@@ -4,7 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class I18nMessageApplication
+class I18nMessageApplication {
+
+}
 
 fun main(args: Array<String>) {
     runApplication<I18nMessageApplication>(*args)
--- src/main/kotlin/it/valeriovaudi/i18nmessage/messages/MessageRepository.kt
+++ src/main/kotlin/it/valeriovaudi/i18nmessage/messages/MessageRepository.kt
@@ -1,9 +1,10 @@
 package it.valeriovaudi.i18nmessage.messages
 
+import java.util.*
+
+typealias Messages = Map<String, String>
 
 interface MessageRepository {
 
-    fun findOne(application: String, family: String, key: String): Map<String, String>
-
-    fun find(application: String, family: String): Map<String, String>
+    fun find(application: String, language: Locale): Messages
 }
