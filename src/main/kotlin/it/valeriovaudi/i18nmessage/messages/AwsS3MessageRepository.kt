f1e01a43e0c624e4536ee52ed8c63adb317db358
mrFlick72 <valerio.vaudi@gmail.com>
2019-09-28 13:43:43 +0200

    cache the S3 properties file

--- src/main/kotlin/it/valeriovaudi/i18nmessage/messages/AwsS3MessageRepository.kt
+++ src/main/kotlin/it/valeriovaudi/i18nmessage/messages/AwsS3MessageRepository.kt
@@ -3,12 +3,16 @@ package it.valeriovaudi.i18nmessage.messages
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.ObjectListing
 import com.amazonaws.services.s3.model.S3ObjectSummary
+import org.springframework.cache.annotation.Cacheable
 import java.io.BufferedInputStream
 import java.util.*
 
-class AwsS3MessageRepository(private val s3client: AmazonS3,
-                             private val bucketName: String) : MessageRepository {
+typealias  S3ObjectSummaryPredicate = (S3ObjectSummary) -> Boolean
 
+open class AwsS3MessageRepository(private val s3client: AmazonS3,
+                                  private val bucketName: String) : MessageRepository {
+
+    @Cacheable("i18nMessageBundle", key = "#application + '_' + #language.toString()")
     override fun find(application: String, language: Locale): Messages {
         val allS3MessagesBundle = getAllS3MessagesBundleFor(application)
         val s3MessageBundle = s3MessageBundleFor(allS3MessagesBundle, application, language.toString())
@@ -18,9 +22,15 @@ class AwsS3MessageRepository(private val s3client: AmazonS3,
     private fun s3MessageBundleFor(message: ObjectListing, application: String, language: String) =
             messageKeyFor(application)
                     .let { messagesKey ->
-                        Optional.ofNullable(message.objectSummaries.find { it.key.equals("$messagesKey/messages_$language.properties") })
-                                .orElse(message.objectSummaries.find { it.key.equals("$messagesKey/messages.properties") })
+                        Optional.ofNullable(message.objectSummaries.find(resourceBundleFinderPredicate(messagesKey, language)))
+                                .orElse(message.objectSummaries.find(defaultRsourceBundleFinderPredicate(messagesKey)))
                     }
+
+    private fun defaultRsourceBundleFinderPredicate(messagesKey: String): S3ObjectSummaryPredicate =
+            { it.key.equals("$messagesKey/messages.properties") }
+
+    private fun resourceBundleFinderPredicate(messagesKey: String, language: String): S3ObjectSummaryPredicate =
+            { it.key.equals("$messagesKey/messages_$language.properties") }
 
     private fun s3MessageBundleContentFor(message: S3ObjectSummary) =
             s3client.getObject(bucketName, message.key)
