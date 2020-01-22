346242a94c96ba97b5f45f62f1eb25668db115ca
mrFlick72 <valerio.vaudi@gmail.com>
2020-01-22 01:08:25 +0100

    migration from blocking to no-blocking IO

--- src/main/kotlin/it/valeriovaudi/i18nmessage/messages/MessageRepository.kt
+++ src/main/kotlin/it/valeriovaudi/i18nmessage/messages/MessageRepository.kt
@@ -1,10 +1,11 @@
 package it.valeriovaudi.i18nmessage.messages
 
+import reactor.core.publisher.Mono
 import java.util.*
 
 typealias Messages = Map<String, String>
 
 interface MessageRepository {
 
-    fun find(application: String, language: Locale): Messages
+    fun find(application: String, language: Locale): Mono<Messages>
 }
