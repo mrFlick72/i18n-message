ad7e767c9b11c7de9b8f7e0b9a1360a5e54774fa
mrFlick72 <valerio.vaudi@gmail.com>
2020-06-26 22:44:50 +0200

    error fix

--- src/main/kotlin/it/valeriovaudi/i18nmessage/messages/MessageRepository.kt
+++ src/main/kotlin/it/valeriovaudi/i18nmessage/messages/MessageRepository.kt
@@ -1,13 +1,8 @@
 package it.valeriovaudi.i18nmessage.messages
 
-import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.model.ObjectListing
-import com.amazonaws.services.s3.model.S3ObjectSummary
-import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 import reactor.core.publisher.toMono
-import java.io.BufferedInputStream
 import java.io.ByteArrayInputStream
 import java.util.*
 
@@ -25,30 +20,34 @@ class RestMessageRepository(
 
     override fun find(application: String, language: Locale): Mono<Messages> =
             findFor { baseUrlFor(application, language.toString()) }
-                    .switchIfEmpty(findFor { baseUrlFor(application) })
+                    .onErrorResume { findFor { baseUrlFor(application) } }
 
-    private fun findFor(baseURL: () -> String): Mono<Messages> {
-        return client.baseUrl(baseURL())
-                .build()
-                .get()
-                .accept(MediaType.APPLICATION_OCTET_STREAM)
-                .exchange()
-                .flatMap { it.bodyToMono(ByteArray::class.java) }
-                .flatMap { loadBundle(it) }
-    }
+    private fun findFor(baseURL: () -> String): Mono<Messages> =
+            client.baseUrl(baseURL())
+                    .build()
+                    .get()
+                    .exchange()
+                    .flatMap {
+                        when (it.statusCode().is2xxSuccessful) {
+                            true -> it.bodyToMono(ByteArray::class.java)
+                            false -> Mono.error(RuntimeException("language not found"))
+                        }
+                    }
+                    .flatMap(this::loadBundle)
 
 
     private fun loadBundle(data: ByteArray): Mono<Map<String, String>> =
             Mono.defer { Properties().toMono() }
                     .map { props ->
-                        ByteArrayInputStream(data).use { props.load(it) };
+                        ByteArrayInputStream(data).use { props.load(it) }
                         props
                     }
                     .map { it.toMap() as Map<String, String> }
+
 
     private fun baseUrlFor(application: String) =
             "$repositoryServiceUrl/documents/$registrationName?path=$application&fileName=messages&fileExt=properties"
 
     private fun baseUrlFor(application: String, lang: String) =
-            "$repositoryServiceUrl/documents/$registrationName?path=$application&fileName=messages_lang&fileExt=properties"
+            "$repositoryServiceUrl/documents/$registrationName?path=$application&fileName=messages_$lang&fileExt=properties"
 }
