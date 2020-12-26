412c4f8b875c6f19da93e025d216267edc70c2f3
mrFlick72 <valerio.vaudi@gmail.com>
2020-12-26 01:03:12 +0100

    logging system

--- src/configuration/manager.go
+++ src/configuration/manager.go
@@ -3,7 +3,6 @@ package configuration
 import (
 	"github.com/Piszmog/cloudconfigclient"
 	"github.com/spf13/viper"
-	"github/mrflick72/i18n-message/src/internal/logging"
 	"net/http"
 	"os"
 	"sync"
@@ -42,8 +41,6 @@ func (manager *Manager) Init(wg *sync.WaitGroup) {
 	manager.viper.AddConfigPath(".")
 	manager.viper.AutomaticEnv()
 	manager.viper.ReadInConfig()
-
-	logging.LogDebugFor(manager.viper)
 	wg.Done()
 }
 
