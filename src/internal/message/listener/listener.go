9c823078d19e154a1eb62fb5144fe4870f3c6b9d
mrFlick72 <valerio.vaudi@gmail.com>
2021-01-01 02:38:22 +0100

    refactoring

--- src/internal/message/listener/listener.go
+++ src/internal/message/listener/listener.go
@@ -6,7 +6,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
-	//"github.com/yalp/jsonpath"
 	"github/mrflick72/i18n-message/src/internal/logging"
 	"sync"
 	"time"
