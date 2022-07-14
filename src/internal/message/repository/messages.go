10ce939ee118296a54ebb7c97615f41df6e68283
mrflick72 <valerio.vaudi@gmail.com>
2022-07-14 21:07:06 +0200

    language fallback use case

--- src/internal/message/repository/messages.go
+++ src/internal/message/repository/messages.go
@@ -34,8 +34,53 @@ func (repository *S3MessageRepository) Find(application string, language string,
 	if language != "" {
 		language = "_" + language
 	}
-	objectKey := fmt.Sprintf("%s/messages%s.properties", application, language)
+	objectKey := objectKeyFor(application, language)
 
+	object, err := downloadFile(repository, objectKey)
+
+	if err != nil {
+		fallBackObjectKey := objectKeyFor(application, "")
+		object, err = downloadFile(repository, fallBackObjectKey)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	bytes := objectContentFor(object)
+
+	properties, err := properties.Load(bytes, properties.UTF8)
+
+	if err != nil {
+		return nil, err
+	}
+
+	message := messageFrom(properties)
+	return &message, nil
+}
+
+func messageFrom(properties *properties.Properties) Message {
+	var message = make(Message)
+	message = properties.Map()
+	return message
+}
+
+func objectContentFor(object *s3.GetObjectOutput) []byte {
+	buf := new(bytes.Buffer)
+
+	body := object.Body
+	buf.ReadFrom(body)
+	defer body.Close()
+
+	bytes := buf.Bytes()
+	return bytes
+}
+
+func objectKeyFor(application string, language string) string {
+	return fmt.Sprintf("%s/messages%s.properties", application, language)
+}
+
+func downloadFile(repository *S3MessageRepository, objectKey string) (*s3.GetObjectOutput, error) {
 	object, err := repository.Client.GetObject(&s3.GetObjectInput{
 		Bucket: &repository.BuketName,
 		Key:    &objectKey,
@@ -45,18 +90,5 @@ func (repository *S3MessageRepository) Find(application string, language string,
 		return nil, err
 	}
 
-	buf := new(bytes.Buffer)
-
-	body := object.Body
-	buf.ReadFrom(body)
-	defer body.Close()
-
-	properties, err := properties.Load(buf.Bytes(), properties.UTF8)
-	if err != nil {
-		return nil, err
-	}
-
-	var message = make(Message)
-	message = properties.Map()
-	return &message, nil
+	return object, nil
 }
